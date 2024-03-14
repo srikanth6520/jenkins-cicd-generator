92a58a47b9a6540270e5ccb0b2c32c8ae84bff35
srikanth6520 <84612610+srikanth6520@users.noreply.github.com>
2024-03-14 14:12:18 +0530

    Update jenkins-pipeline-java-nodejs.py

--- jenkins-pipeline-java-nodejs.py
+++ jenkins-pipeline-java-nodejs.py
@@ -2,25 +2,26 @@ import argparse
 from jinja2 import Environment, FileSystemLoader
 import os
 
-def generate_build_pipeline(repo_url, branch, language, build_tool, namespace, deploy=True):
+def generate_build_pipeline(repo_url, branch, build_tool, docker_image, docker_tag, dockerhub_credentials_id, namespace, job_name, manifests_dir, kube_credentials_id):
     # Set up Jinja environment
     template_dir = os.path.join(os.path.dirname(__file__), 'templates')
     env = Environment(loader=FileSystemLoader(template_dir))
-    # Load Jinja template based on language
-    if language == 'java':
-        template = env.get_template('java_pipeline.j2')
-    elif language == 'javascript':
-        template = env.get_template('nodejs_pipeline.j2')
-    else:
-        raise ValueError(f'Unsupported language: {language}')
+    
+    # Load Jinja template
+    template = env.get_template('template_file.j2')
 
     # Render template with provided inputs
     pipeline_script = template.render(
-        repo_url=repo_url,
-        branch=branch,
-        build_tool=build_tool,
-        namespace=namespace,
-        deploy=deploy
+        GIT_REPO=repo_url,
+        GIT_BRANCH=branch,
+        BUILD_TOOL=build_tool,
+        DOCKER_IMAGE=docker_image,
+        DOCKER_TAG=docker_tag,
+        DOCKERHUB_CREDENTIALS_ID=dockerhub_credentials_id,
+        NAMESPACE=namespace,
+        JOB_NAME=job_name,
+        MANIFESTS_DIR=manifests_dir,
+        KUBE_CREDENTIALS_ID=kube_credentials_id
     )
 
     return pipeline_script
@@ -30,10 +31,14 @@ def main():
     parser = argparse.ArgumentParser(description='Generate Jenkins pipeline file')
     parser.add_argument('-r', '--repo-url', type=str, help='Repository URL', dest='repo_url', required=True)
     parser.add_argument('-b', '--branch', type=str, help='Branch', dest='branch', default='main')
-    parser.add_argument('-l', '--language', type=str, help='Language', dest='language', required=True, choices=['java', 'javascript'])
     parser.add_argument('-t', '--build-tool', type=str, help='Build tool', dest='build_tool', required=True, choices=['maven', 'gradle', 'npm', 'yarn'])
+    parser.add_argument('--docker-image', type=str, help='Docker Image', dest='docker_image', default='my-docker-image')
+    parser.add_argument('--docker-tag', type=str, help='Docker Tag', dest='docker_tag', default='latest')
+    parser.add_argument('--dockerhub-credentials-id', type=str, help='Docker Hub Credentials ID', dest='dockerhub_credentials_id', default='dockerhub-credentials')
     parser.add_argument('-n', '--namespace', type=str, help='Namespace', dest='namespace', required=True)
-    parser.add_argument('-d', '--deploy', action='store_true', help='Deploy Helm chart', dest='deploy')
+    parser.add_argument('--job-name', type=str, help='Job Name', dest='job_name', default='test')
+    parser.add_argument('--manifests-dir', type=str, help='Directory to store manifest files', dest='manifests_dir', default='manifests')
+    parser.add_argument('--kube-credentials-id', type=str, help='Kubernetes Credentials ID', dest='kube_credentials_id', default='k8s-credentials')
     parser.add_argument('-o', '--output-file', type=str, help='Output file', dest='output_file', default='default_pipeline.groovy')
     
     args = parser.parse_args()
@@ -42,10 +47,14 @@ def main():
     pipeline_script = generate_build_pipeline(
         args.repo_url,
         args.branch,
-        args.language,
         args.build_tool,
+        args.docker_image,
+        args.docker_tag,
+        args.dockerhub_credentials_id,
         args.namespace,
-        args.deploy
+        args.job_name,
+        args.manifests_dir,
+        args.kube_credentials_id
     )
 
     # Write the generated pipeline script to the output file
