8f5e61e6d15f96033af54ccbb803d869dbed3569
srikanth6520 <84612610+srikanth6520@users.noreply.github.com>
2024-03-18 17:34:28 +0530

    Update jenkins-pipeline-java-nodejs.py

--- jenkins-pipeline-java-nodejs.py
+++ jenkins-pipeline-java-nodejs.py
@@ -2,18 +2,20 @@ import argparse
 from jinja2 import Environment, FileSystemLoader
 import os
 
-def generate_build_pipeline(repo_url, branch, build_tool, dockerfile_dir, docker_image, docker_tag, dockerhub_credentials_id, namespace, job_name, manifests_dir, kube_credentials_id, language):
+def generate_build_pipeline(repo_url, branch, build_tool, dockerfile_dir, docker_image, docker_tag, dockerhub_credentials_id, namespace, job_name, manifests_dir, kube_credentials_id):
     # Set up Jinja environment
     template_dir = os.path.join(os.path.dirname(__file__), 'templates')
     env = Environment(loader=FileSystemLoader(template_dir))
     
-    # Load Jinja template based on the language
-    if language == 'java':
-        template = env.get_template('java_pipeline.j2')
-    elif language == 'javascript':
+    # Load Jinja template based on the build tool
+    if build_tool == 'maven':
+        template = env.get_template('maven_pipeline.j2')
+    elif build_tool == 'gradle':
+        template = env.get_template('gradle_pipeline.j2')
+    elif build_tool in ['npm', 'yarn']:
         template = env.get_template('nodejs_pipeline.j2')
     else:
-        raise ValueError(f'Unsupported language: {language}')
+        raise ValueError(f'Unsupported build tool: {build_tool}')
 
     # Render template with provided inputs
     pipeline_script = template.render(
@@ -37,7 +39,7 @@ def main():
     parser = argparse.ArgumentParser(description='Generate Jenkins pipeline file')
     parser.add_argument('--repo-url', type=str, help='Repository URL', default='https://example.com')
     parser.add_argument('--branch', type=str, help='Branch', default='main')
-    parser.add_argument('--build-tool', type=str, help='Build tool', default='maven', choices=['maven', 'gradle'])
+    parser.add_argument('--build-tool', type=str, help='Build tool', default='maven', choices=['maven', 'gradle', 'npm', 'yarn'])
     parser.add_argument('--dockerfile-dir', type=str, help='Directory where Dockerfile is located', default='path/to/dockerfiles')
     parser.add_argument('--docker-image', type=str, help='Docker Image', default='my-docker-image')
     parser.add_argument('--docker-tag', type=str, help='Docker Tag', default='latest')
@@ -46,7 +48,6 @@ def main():
     parser.add_argument('--job-name', type=str, help='Job Name', default='test')
     parser.add_argument('--manifests-dir', type=str, help='Directory to store manifest files', default='manifests')
     parser.add_argument('--kube-credentials-id', type=str, help='Kubernetes Credentials ID', default='k8s-credentials')
-    parser.add_argument('--language', type=str, help='Programming language', choices=['java', 'javascript'], required=True)
     parser.add_argument('--output-file', type=str, help='Output file', default='default_pipeline.groovy')
 
     args = parser.parse_args()
@@ -63,8 +64,7 @@ def main():
         args.namespace,
         args.job_name,
         args.manifests_dir,
-        args.kube_credentials_id,
-        args.language
+        args.kube_credentials_id
     )
 
     # Write the generated pipeline script to the output file
